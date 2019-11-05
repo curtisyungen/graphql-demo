58be38a45b4b0c90794f931550509b3ea6f868ae
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-11-05 15:57:10 -0800

    initial commit

---
+++ public/precache-manifest.5598ea52af7c11c62314bee58da23ed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d66b49c9f3ac3a80f15894a8a7298a93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12f4a6ef4d21245b673a",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "8a9d3f38e7de11fd0415",
+    "url": "/static/js/2.e13212e5.chunk.js"
+  },
+  {
+    "revision": "12f4a6ef4d21245b673a",
+    "url": "/static/js/main.5b1efd51.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "7dab9996f15513443ab7ec4d1c04cab9",
+    "url": "/static/media/logo.7dab9996.png"
+  }
+]);
